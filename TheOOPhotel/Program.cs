43b9866629a50b90c1e82b89ab7322295791ef4d
simon <simon.tiger.lindstrom@gmail.com>
2024-09-24 13:11:20 +0200

    Ny klass

--- TheOOPhotel/Program.cs
+++ TheOOPhotel/Program.cs
@@ -11,7 +11,15 @@ namespace TheOOPhotel
         static void Main(string[] args)
         {
             Console.WriteLine("Hej! Vad är ditt för och efternamn?");
-            string guestName = Console.ReadLine();
+            string name = Console.ReadLine();
+
+            Console.Write("Ange din Epost: ");
+            string email = Console.ReadLine();
+
+            Console.Write("Ange ditt telefonnummer: ");
+            string phone = Console.ReadLine();
+
+            Person guest = new Person(name, email, phone);
 
             Console.WriteLine("Från när vill du boka ditt rum?: ");
 
@@ -24,7 +32,7 @@ namespace TheOOPhotel
             Console.WriteLine("Hur många dagar vill du boka?");
             int lengthOfStayInDays = int.Parse(Console.ReadLine());
 
-            HotelBooking booking = new HotelBooking(guestName, startDate, lengthOfStayInDays);
+            HotelBooking booking = new HotelBooking(guest, startDate, lengthOfStayInDays);
             booking.ShowBookingDetails();
 
             Console.WriteLine("Vill du ändra dina bokade dagar?");
@@ -33,7 +41,7 @@ namespace TheOOPhotel
             {
                 Console.WriteLine("Hur många dagar vill du lägga till?: ");
                 lengthOfStayInDays = int.Parse(Console.ReadLine());
-                HotelBooking updatedBooking = new HotelBooking(guestName, startDate, lengthOfStayInDays);
+                HotelBooking updatedBooking = new HotelBooking(guest, startDate, lengthOfStayInDays);
                 updatedBooking.ShowBookingDetails();
             }
             else
@@ -45,26 +53,44 @@ namespace TheOOPhotel
     }
     public class HotelBooking
     {
-        public string GuestName { get; set; }
+        public Person Guest { get; set; }
         public DateTime StartDate { get; set; }
         public DateTime EndDate { get; set; }
 
         public int PricePerNigth;
-        public HotelBooking(string guestName, DateTime startDate, int lengthOfStayInDays)
+        public HotelBooking(Person guest, DateTime startDate, int lengthOfStayInDays)
         {
-            GuestName = guestName;
+            Guest = guest;
             StartDate = startDate.Add(new TimeSpan(15,0,0));
             EndDate = startDate.AddDays(lengthOfStayInDays).Date.Add(new TimeSpan(12, 0, 0));
             PricePerNigth = lengthOfStayInDays * 1999;
         }
         public void ShowBookingDetails()
         {
-            Console.WriteLine($"Gästnamn: {GuestName}");
+            Console.WriteLine($"Gästnamn: {Guest.Name}");
             Console.WriteLine($"Incheckning: {StartDate}");
             Console.WriteLine($"Utcheckning: {EndDate}");
             Console.WriteLine("Pris per natt: 1999 kr");
             Console.WriteLine($"Pris för vistelse: {PricePerNigth} kr"); 
         }
 
+    }
+    public class Person
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public Person(string name, string email, string phone)
+        {
+            Name = name;
+            Email = email;
+            Phone = phone;
+        }
+        public void personinfo()
+        {
+            Console.WriteLine($"Namn: {Name}");
+            Console.WriteLine($"Epost: {Email}");
+            Console.WriteLine($"Nummer: {Phone}");
+        }
     }
 }
